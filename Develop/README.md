a220c9d6c62b9ba9dedfb8fe6243ec5be0e7a59c
stewk033 <kevin.c.stewart04@gmail.com>
2021-11-30 15:00:34 -0500

    prompts update and screen capture

---
+++ Develop/README.md
@@ -0,0 +1,39 @@
+# README Generator
+
+## Description
+
+ This is a command-line application that will generate a high-quality README file using input from the user.
+
+## Table of Contents
+
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Contribution](#contribution)
+ - [License](#license)
+ - [Test](#test)
+ - [Questions](#questions)
+## Installation
+
+ To create your own README file, first run rpm install to install the npm package and inquirer dependencies. To run the application, type node index.js in your command-line.
+
+## Usage
+
+ This application is used to generate a high-quality professional README file. See the following demonstration:
+
+## Contribution
+
+ Kevin Stewart with the starting code: https://github.com/coding-boot-camp/potential-enigma
+
+## License
+
+ [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+
+## Tests
+
+ If you’re interested in using this application, be sure to test it in your own console. Don’t forget to install the npm package using npm install followed by npm i inquirer.
+
+## Questions
+
+ Contact Me:
+ GitHub: https://github.com/stewk033
+ Email: kev_ace04@yahoo.com
--- Develop/index.js
+++ Develop/index.js
@@ -1,33 +1,112 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const renderLicenseBadge = license => {
+    if (license === 'BSD') {
+      return `\r[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+    }
+    else if (license === 'MIT') {
+      return `\r[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    }
+    else if (license === 'GNU') {
+      return `\r[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    }
+  }
 
-const prompt = require('prompt-sync')();
-
-// var fs = require('fs');
-
-// fs.writeFileSync('README.md',) {
-
-// }
 // TODO: Create an array of questions for user input
-const questions = [
-    'What is the title of your project?',
-    'Enter a description of your project.',
-    'What are the installation instructions?',
-    'What is the usage information?',
-    'What are the contribution guidelines?',
-    'What are the test instructions?',
-    'Choose a license for your application.',
-    'What is your GitHub username?',
-    'What is your email address?',
+const info = [
+    {
+        name: "title",
+        type: "input",
+        message: "What is the title?",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log("You must have a title");
+                return false;
+            }
+        }
+    },
+    {
+        name: "description",
+        type: "input",
+        message: "What is the description?",
+    },
+    {
+        name: "install",
+        type: "input",
+        message: "What are the installation instructions?",
+    },
+    {
+        name: "usage",
+        type: "input",
+        message: "What is the usage information?",
+    },
+    {
+        name: "contribution",
+        type: "input",
+        message: "What are the contribution guidelines?",
+    },
+    {
+        name: "test",
+        type: "input",
+        message: "What are the test instructions?",
+    },
+    {
+        name: "license",
+        type: "list",
+        message: "Do you want to include a license?",
+        choices: ["BSD", "MIT", "GNU", "none"],
+    },
+    {
+        name: "username",
+        type: "input",
+        message: "Enter your GitHub username",
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log("Please enter a GitHub username");
+                return false;
+            }
+        }
+    },
+    {
+        name: "email",
+        type: "input",
+        message: "What is my email address?",
+    },
 ];
 
-const name = prompt('What is your name?');
-console.log(`Hey there ${name}`);
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const content = data.title + data.description + data.toc + data.installation + data.usage + data.contribution + data.license + data.test + data.questions
+    fs.writeFile(fileName, content, (err) => {
+        if (err) throw err;
+    
+        console.log("The file was succesfully saved!");
+    }); 
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(info)
+    .then(answers => {
+        const data = {
+            title: `# ${answers.title}\n\n`,
+            description: `## Description\n\n ${answers.description}\n\n`,
+            toc: `## Table of Contents\n\n - [Installation](#installation)\n - [Usage](#usage)\n - [Contribution](#contribution)\n - [License](#license)\n - [Test](#test)\n - [Questions](#questions)\n`,
+            installation: `## Installation\n\n ${answers.install}\n\n`,
+            usage: `## Usage\n\n ${answers.usage}\n\n`,
+            contribution: `## Contribution\n\n ${answers.contribution}\n\n`,
+            license: `## License\n\n ${renderLicenseBadge(answers.license)}\n\n`,
+            test: `## Tests\n\n ${answers.test}\n\n`,
+            questions: `## Questions\n\n Contact Me:\n GitHub: https://github.com/${answers.username}\n Email: ${answers.email}\n`,
+        }
+        writeToFile("README.md", data)
+    })
+}
 
 // Function call to initialize app
 init();
